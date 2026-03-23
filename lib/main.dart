e6c8b9fd66983b614ab1d7b4036f09e49a89f84f
Mahdi <96683151+Mahdi-Joumaa@users.noreply.github.com>
2026-03-23 21:24:20 +0200

    changed the name of home to splash since it is really not a home screen, but the screen before the auth starts

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:spendr/models/budget_model.dart';
-import 'package:spendr/models/expense_model.dart';
-import 'package:spendr/models/user_model.dart';
 import 'firebase_options.dart';
 
 void main() async {
