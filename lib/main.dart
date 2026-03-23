d223f7ad88755acbcd2347331b113ef92b0097a4
Mahdi <96683151+Mahdi-Joumaa@users.noreply.github.com>
2026-03-23 21:28:07 +0200

    set up the main with routes (will have errors since the screens arent yet built) so ill be commenting as i go .

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,45 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
+import 'utils/theme.dart';
+import 'screens/splash/splash_screen.dart';
+import 'screens/auth/login_screen.dart';
+import 'screens/auth/signup_screen.dart';
+import 'screens/dashboard/dashboard_screen.dart';
+import 'screens/add_expense/add_expense.dart';
+import 'screens/history/history_screen.dart';
+import 'screens/budget/budget_screen.dart';
+import 'screens/profile/profile_screen.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
- 
-  runApp(MaterialApp(
-    home: Container(
-      child: Text('Spendr'),
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(
+    ProviderScope(
+      child: MyApp(),
     ),
-  ));
+  );
 }
 
-
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spendr',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.dark,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/login': (context) => LoginScreen(),
+        '/signup': (context) => SignupScreen(),
+        '/dashboard': (context) => DashboardScreen(),
+        '/add-expense': (context) => AddExpenseScreen(),
+        '/history': (context) => HistoryScreen(),
+        '/budgets': (context) => BudgetsScreen(),
+        '/profile': (context) => ProfileScreen(),
+      },
+    );
+  }
+}
