8372eeabd9fdfe6e179b1a966554236343ec8a17
Mahdi <96683151+Mahdi-Joumaa@users.noreply.github.com>
2026-03-31 19:10:51 +0300

    updated dependencies for google sign in

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,10 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:spendr/models/user_model.dart';
 
 class AuthService {
   final _auth = FirebaseAuth.instance;
   final _db = FirebaseFirestore.instance;
+  final _googleSignIn = GoogleSignIn();
+
+
+
+   // Google Sign-In);
 
   //sign up
   Future<UserModel?> signUp(String name, String email, String password) async {
